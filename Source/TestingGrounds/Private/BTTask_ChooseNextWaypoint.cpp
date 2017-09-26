b237caed88f63fc23bb9e2e8a40fb0a0078156fe
ddelvall <diegodvi@gmail.com>
2017-09-26 13:10:13 -0300

    Staged files forgotten

--- Source/TestingGrounds/Private/BTTask_ChooseNextWaypoint.cpp
+++ Source/TestingGrounds/Private/BTTask_ChooseNextWaypoint.cpp
@@ -2,6 +2,11 @@
 
 #include "BTTask_ChooseNextWaypoint.h"
 
+EBTNodeResult::Type UBTTask_ChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	UE_LOG(LogTemp, Warning, TEXT("AI in C++"));
+	return EBTNodeResult::Succeeded;
+}
 
 
 
--- Source/TestingGrounds/Public/BTTask_ChooseNextWaypoint.h
+++ Source/TestingGrounds/Public/BTTask_ChooseNextWaypoint.h
@@ -14,7 +14,8 @@ class TESTINGGROUNDS_API UBTTask_ChooseNextWaypoint : public UBTTaskNode
 {
 	GENERATED_BODY()
 	
-	
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+
 	
 	
 };
