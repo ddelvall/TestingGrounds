c22305bb509d1eba5ad8266ce889f8883316ff0c
ddelvall <diegodvi@gmail.com>
2017-09-29 13:58:20 -0300

    Nextwaypoint from BP to c++, next: composition over inheritance

--- Source/TestingGrounds/Private/BTTask_ChooseNextWaypoint.cpp
+++ Source/TestingGrounds/Private/BTTask_ChooseNextWaypoint.cpp
@@ -1,14 +1,32 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "BTTask_ChooseNextWaypoint.h"
+#include "AIController.h"
+#include "PatrollingGuard.h" // TODO remove coupling
 #include "BehaviorTree/BlackboardComponent.h"
 
 EBTNodeResult::Type UBTTask_ChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
-	
+	// Get patrol points
+	auto AIController = OwnerComp.GetAIOwner();
+	auto ControlledPawn = AIController->GetPawn();
+
+	auto PatrollingGuard = Cast<APatrollingGuard>(ControlledPawn);
+	auto PatrolPoints = PatrollingGuard->PatrolPointsCPP;
+
+	// Set next waypoint
 	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
 	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
-	UE_LOG(LogTemp, Warning, TEXT("Waypoint Index: %i"), Index);
+
+	BlackboardComp->SetValueAsObject(WaypointKey.SelectedKeyName, PatrolPoints[Index]);
+	// TODO protect agains empty patrols
+
+	// Cycle the index
+	auto NextIndex = (Index + 1) % PatrolPoints.Num();
+	BlackboardComp->SetValueAsInt(IndexKey.SelectedKeyName, NextIndex);
+
+//	UE_LOG(LogTemp, Warning, TEXT("Waypoint Index: %i"), Index);
+
 	return EBTNodeResult::Succeeded;
 }
 
--- Source/TestingGrounds/Public/BTTask_ChooseNextWaypoint.h
+++ Source/TestingGrounds/Public/BTTask_ChooseNextWaypoint.h
@@ -19,5 +19,8 @@ class TESTINGGROUNDS_API UBTTask_ChooseNextWaypoint : public UBTTaskNode
 protected:
 	UPROPERTY(EditAnywhere, Category = "Blackboard")
 	struct FBlackboardKeySelector IndexKey;
+
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	struct FBlackboardKeySelector WaypointKey;
 	
 };
